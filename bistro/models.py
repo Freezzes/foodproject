c4662d774f076d609c876d777cfdb6c25b639623
wanvipa <wanvipa1906@gmail.com>
2019-02-16 14:24:40 +0700

    create model

--- bistro/models.py
+++ bistro/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Shop(models.Model):
+    shop_name = models.CharField(maxlength=200)
+    category = models.CharField(maxlength=20)
+
+class review(models.Model):
+    name = models.ForeignKey(Shop)
+    comment = models.CharField(max_length=200)
+    score = models.IntegerField(default=0)
