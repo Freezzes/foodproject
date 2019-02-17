13965b176ef85622ba3dcb00a719e599f154e680
wanvipa <wanvipa1906@gmail.com>
2019-02-17 16:21:43 +0700

    create template

--- bistro/urls.py
+++ bistro/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 app_name = 'bistro'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.DetialView.as_view(), name='index'),
     path('search/',views.search,name='search'),
     path('searchname/',views.showsearch,name='searchname'),
-    path('find/',views.find,name='find'),
-    path('random/',views.random,name='random'),
+    path('find/', views.IndexView.as_view(), name='find'),
+    # path('find/',views.find,name='find'),
+    path('random/',views.randomshop,name='random'),
     path('review/',views.review_bistro,name='review'),
 
 ]
--- bistro/views.py
+++ bistro/views.py
@@ -3,10 +3,23 @@ from django.shortcuts import get_object_or_404,render
 from django.http import Http404
 from django.urls import reverse
 from .models import Shop,review
+from django.views import generic
+import random
 
 # Create your views here.
-def index(request):
-    return render(request,'bistro/index.html')
+class IndexView(generic.ListView):
+    template_name = 'bistro/find.html'
+    context_object_name = 'latest_shop_list'
+
+    def get_queryset(self):
+        return Shop.objects.order_by('category')
+
+class DetialView(generic.ListView):
+    template_name = 'bistro/index.html'
+    context_object_name = 'latest_shop_list'
+
+    def get_queryset(self):
+        return Shop.objects.order_by('shop_name')
 
 def search(request):
     return render(request,'bistro/search.html')
@@ -23,8 +36,12 @@ def showsearch(request):
 def find(request):
     return render(request,'bistro/find.html')
 
-def random(request):
-    return HttpResponse("random bistro")
+def randomshop(request):
+    shops = Shop.objects.all()
+    random_shop = Shop.objects.all(random=Shop.shop_name)
+    cont = {'rs':random_shop}
+    return render(request,'bistro/random.html')
 
 def review_bistro(request):
-    return HttpResponse("search from name")
+    return render(request,'bistro/review.html')
+
